5fb4151d9a5371c373478aac799e4e2a04f109e9
hoangtran <hoangtx2710@gmail.com>
2021-05-12 15:41:06 +0700

    implement calculator

--- src/services/calculator-service.js
+++ src/services/calculator-service.js
@@ -1,14 +1,17 @@
 import client from "./http-service";
-import { BASE_URL_API } from "utils/constants";
+import { BASE_URL_API } from "../utils/constants";
 
+// get list products
 export function getProducts() {
   return client.get(`${BASE_URL_API}/products`);
 }
 
+// get list locations
 export function getLocations() {
   return client.get(`${BASE_URL_API}/locations`);
 }
 
+// add product to card
 export function addToCard(request) {
   return client.post(`${BASE_URL_API}/cart`, request);
 }
--- src/services/http-service.js
+++ src/services/http-service.js
@@ -1,11 +1,13 @@
 import axios from "axios";
-import { REQUEST_TIME_OUT, BASE_URL_API } from "utils/constants";
+import { REQUEST_TIME_OUT, BASE_URL_API } from "../utils/constants";
 
+// generate axios
 const client = axios.create({
   timeout: REQUEST_TIME_OUT,
   baseURL: BASE_URL_API
 });
 
+// config request of axios
 client.interceptors.request.use(
   (config) => {
     return config;
@@ -13,6 +15,7 @@ client.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+// config response of axios
 client.interceptors.response.use(
   (response) => {
     return response.data;
